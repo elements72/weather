5cabca6dedb1b9737cd0d5443754ef184b8b3895
elements72 <antonio.lopez.8523@gmail.com>
2021-03-08 20:32:29 +0100

    Added images

---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""
+Shows the weather for the selected city.
+
+The extension shows the main informations about the wheater of the selected city today.
+
+Synopsis: <trigger> [city]
+"""
+__doc__     = "Show the weather of the selected city"
+__title__   = "Weather"
+__version__ = "0.1"
+__triggers__ = "wt "
+__authors__  = "elements72, adrianoRieti"
+
+def initialize():
+    pass
+
+def handleQuery(Query):
+    pass    
