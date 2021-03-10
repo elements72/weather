6c4a9ea1196d55f653965288f1bd2b041e3dcb29
elements72 <antonio.lopez.8523@gmail.com>
2021-03-10 20:04:00 +0100

    Implemented the Api requestes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+from datetime import date
+from time import sleep
+from albert import *
+import requests
+import json
+from geopy import *
+from geopy.extra.rate_limiter import RateLimiter
 """
 Shows the weather for the selected city.
 
@@ -6,13 +13,56 @@ The extension shows the main informations about the wheater of the selected city
 Synopsis: <trigger> [city]
 """
 __doc__     = "Show the weather of the selected city"
-__title__   = "Weather"
-__version__ = "0.1"
+__title__   = "weather"
+__version__ = "0.1.0"
 __triggers__ = "wt "
 __authors__  = "elements72, adrianoRieti"
+__py_deps__ = "geopy"
 
 def initialize():
     pass
 
-def handleQuery(Query):
-    pass    
+def handleQuery(query):
+    if not query.isTriggered:
+        return
+    city = query.string
+    debug(city)
+    data = getData(city, query)
+    if data != None:
+        return make_item(city, data)
+    else:
+        return
+        #return make_item("City not found", None)
+
+def getData(city, query):
+    if query.isValid:
+        geocoder = Nominatim(user_agent="http")
+        location = geocoder.geocode(city)
+        if location != None:
+            url = "http://www.7timer.info/bin/api.pl?lon={0}&lat={1}&lang=it&product=civillight&output=json".format(location.longitude, location.latitude)
+            response = requests.get(url)
+            data = json.loads(response.text)
+            return data["dataseries"][0]
+    return None
+
+
+def make_item(city, data):
+    text = city
+    temperature = ""
+    imgDir = "./images/"
+    iconPath = imgDir + "warning.png"
+    iconPaths = {"clear":{"iconPath": "sun.png"}, "pcloudy":{"iconPath": "cloud.png"}, "mcloudy":{"iconPath": "cloud.png"}, "cloudy":{"iconPath": "044-cloudy-2.png"},
+                "humid":{"iconPath": "027-humidity.png"}, "lightrain":{"iconPath": "022-rain-1.png"}, "oshower": {"iconPath": "030-storm.png"}, 
+                "ishower":{"iconPath": "rain.png"}, "rain":{"iconPath": "rain.png"}, "lightsnow":{"iconPath": "snowy.png"}, 
+                "snow":{"iconPath": "snowy.png"}, "rainsnow":{"iconPath": "017-snowy-1.png"}}
+    if data != None:
+        iconPath = imgDir + iconPaths[data["weather"]]["iconPath"]
+        text = text + " " + data["weather"]
+        temperature = "Max:{}° Min:{}°".format(data["temp2m"]["max"], data["temp2m"]["min"])
+    return [Item(
+        id = __title__,
+        text = text,
+        subtext = temperature,
+        icon = iconPath,
+        actions = [],
+    )]  
